e5b3bde27cafddab5dacae1cc6519003768b72c0
kangjs7854 <kangjs7854@163.com>
2020-08-13 18:52:40 +0800

    http 请求加密加签

--- src/util/http.js
+++ src/util/http.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-07-14 14:34:09
  * @LastEditors: kjs
- * @LastEditTime: 2020-08-10 16:45:09
+ * @LastEditTime: 2020-08-13 18:52:18
  * @FilePath: \vue-h5-template\src\util\http.js
  */
 
@@ -17,19 +17,35 @@ let basePath = '/api/v1'
  * @param {Object} params 请求参数
  */
 const post = (url, params = {}) => {
+  //时间戳
+  const requestTimestamp = new Date().getTime();
+  //唯一的id
+  const requestId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    let r = Math.random() * 16 | 0,
+      v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  })
+  //生成签名
+  const paramsArr = Object.keys(params)
+  let signStr = ''
+  //ASCII排序参数
+  paramsArr.sort()
+  paramsArr.forEach(el => {
+    signStr += `${el}=${params[el] || ''}&`
+  })
+  const key = '' //与服务端协商好的key
+  //最掉最后一个参数的&
+  signStr = signStr.slice(0, -1) + key
+  const sign = CryptoJS.md5(signStr).toString().toUpperCase()
+
   const instance = axios.create({
     timeout: 1000 * 10,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      requestId,
+      requestTimestamp,
+      sign
     }
-  })
-
-  Object.assign(params, {
-    userId: '',
-    session_key: '',
-    platform: '',
-    timesamp: Date.parse(new Date()) / 1000 + '',//时间戳
-    sign:''
   })
 
   let reqUrl = url.includes("http") ? url : basePath + url
@@ -37,7 +53,7 @@ const post = (url, params = {}) => {
   return instance.post(reqUrl, qs.stringify(params)).then((res) => {
     if (!res.data || res.data.code == undefined) {
       // do sometings
-      
+
     }
     return res.data
   }).catch((error) => {
